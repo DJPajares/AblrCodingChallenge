0c83bf04f0c6e916a96782a86aaee9a603c9bc16
DJPajares <dj.pajares@gmail.com>
2023-04-19 22:01:34 +0800

    style: uniform styles

--- containers/BuyScreen.tsx
+++ containers/BuyScreen.tsx
@@ -4,7 +4,8 @@ import {Text, SafeAreaView, StyleSheet} from 'react-native';
 const BuyScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Buy</Text>
+      <Text style={styles.headerText}>Buy screen</Text>
+      <Text style={styles.bodyText}>Purchase items here</Text>
     </SafeAreaView>
   );
 };
@@ -14,5 +15,18 @@ export default BuyScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginHorizontal: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 22,
+    fontWeight: '500',
+    marginVertical: 5,
+  },
+  bodyText: {
+    fontSize: 16,
+    fontWeight: '400',
+    marginVertical: 5,
   },
 });
--- containers/ScanScreen.tsx
+++ containers/ScanScreen.tsx
@@ -4,7 +4,7 @@ import {Text, SafeAreaView, StyleSheet} from 'react-native';
 const ScanScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Scan</Text>
+      <Text style={styles.headerText}>Scanning...</Text>
     </SafeAreaView>
   );
 };
@@ -14,5 +14,13 @@ export default ScanScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginHorizontal: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 22,
+    fontWeight: '500',
+    marginVertical: 5,
   },
 });
