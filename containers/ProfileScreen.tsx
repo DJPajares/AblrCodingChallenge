3a5ef68fed572abbb6030c8d310604260ac4253d
DJPajares <dj.pajares@gmail.com>
2023-04-19 21:59:56 +0800

    feat: profile navigate to notif

--- containers/ProfileScreen.tsx
+++ containers/ProfileScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import {Text, SafeAreaView, StyleSheet} from 'react-native';
+import {Text, SafeAreaView, StyleSheet, TouchableOpacity} from 'react-native';
 
-const ProfileScreen = () => {
+const ProfileScreen = ({navigation}) => {
+  const handleNavigateToNotifications = () => {
+    navigation.navigate('Notifications');
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>ProfileScreen</Text>
+      <Text style={styles.headerText}>Profile screen</Text>
+
+      <TouchableOpacity onPress={handleNavigateToNotifications}>
+        <Text style={styles.bodyText}>Go to Notifications</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -14,5 +22,19 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginHorizontal: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 22,
+    fontWeight: '500',
+    marginVertical: 5,
+  },
+  bodyText: {
+    fontSize: 16,
+    fontWeight: '400',
+    color: 'blue',
+    marginVertical: 5,
   },
 });
