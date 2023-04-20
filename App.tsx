da127ca353a36b75be7ca63149d9b172910ebb48
DJPajares <dj.pajares@gmail.com>
2023-04-20 15:53:35 +0800

    refactor: separate bottom tabs

--- App.tsx
+++ App.tsx
@@ -1,103 +1,11 @@
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import React from 'react';
-import HomeScreen from './containers/HomeScreen';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import BuyScreen from './containers/BuyScreen';
-import ScanScreen from './containers/ScanScreen';
-import TransactionsScreen from './containers/TransactionsScreen';
-import ProfileScreen from './containers/ProfileScreen';
+import BottomTabs from './components/BottomTabs';
 import NotificationsScreen from './containers/NotificationsScreen';
 import TransactionScreen from './containers/TransactionScreen';
-import {StyleSheet, View} from 'react-native';
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
-
-type TabBarIconProps = {
-  name: string;
-  color: string;
-  size: number;
-};
-
-const TabBarIcon = ({name, color, size}: TabBarIconProps) => {
-  return <MaterialCommunityIcons name={name} color={color} size={size} />;
-};
-
-const TabBarMiddleIcon = ({name, color, size}: TabBarIconProps) => {
-  return (
-    <View style={styles.middleButton}>
-      <MaterialCommunityIcons name={name} color={color} size={size} />
-    </View>
-  );
-};
-
-const BottomTabs = () => {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      screenOptions={() => ({
-        tabBarStyle: styles.tabBar,
-      })}>
-      <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-          headerShown: false,
-          tabBarLabel: 'Home',
-          tabBarInactiveTintColor: 'black',
-          tabBarIcon: ({color, size}) =>
-            TabBarIcon({name: 'home-variant', color, size}),
-        }}
-      />
-      <Tab.Screen
-        name="Buy"
-        component={BuyScreen}
-        options={{
-          headerShown: false,
-          tabBarLabel: 'Buy',
-          tabBarInactiveTintColor: 'black',
-          tabBarIcon: ({color, size}) =>
-            TabBarIcon({name: 'storefront', color, size}),
-        }}
-      />
-      <Tab.Screen
-        name="Scan"
-        component={ScanScreen}
-        options={{
-          headerShown: false,
-          tabBarLabel: '',
-          tabBarInactiveTintColor: 'black',
-          tabBarIcon: ({size}) =>
-            TabBarMiddleIcon({name: 'line-scan', color: 'white', size}),
-        }}
-      />
-      <Tab.Screen
-        name="Transactions"
-        component={TransactionsScreen}
-        options={{
-          headerShown: false,
-          tabBarLabel: 'Transactions',
-          tabBarInactiveTintColor: 'black',
-          tabBarIcon: ({color, size}) =>
-            TabBarIcon({name: 'credit-card', color, size}),
-        }}
-      />
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{
-          headerShown: false,
-          tabBarLabel: 'Profile',
-          tabBarInactiveTintColor: 'black',
-          tabBarIcon: ({color, size}) =>
-            TabBarIcon({name: 'account', color, size}),
-        }}
-      />
-    </Tab.Navigator>
-  );
-};
 
 const App = () => {
   return (
@@ -112,19 +20,3 @@ const App = () => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  tabBar: {
-    height: 90,
-    paddingTop: 10,
-  },
-  middleButton: {
-    width: 70,
-    height: 70,
-    borderRadius: 35,
-    backgroundColor: '#43a2ea',
-    justifyContent: 'center',
-    alignItems: 'center',
-    shadowColor: '#000',
-  },
-});
